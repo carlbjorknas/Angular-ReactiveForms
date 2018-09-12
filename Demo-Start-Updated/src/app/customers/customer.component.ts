c0c0787087699ecb64b2091e05fba2286f4808ee
Carl Björknäs <carl.bjorknas@knowit.se>
2018-09-12 09:18:26 +0200

    Adds button for populating form controls with test data.

--- Demo-Start-Updated/src/app/customers/customer.component.ts
+++ Demo-Start-Updated/src/app/customers/customer.component.ts
@@ -23,6 +23,22 @@ export class CustomerComponent implements OnInit {
     })
   }
 
+  populateTestData() {
+    // Use setValue when setting ALL values in the form
+    // this.customerForm.setValue({
+    //   firstName: 'Jack',
+    //   lastName: 'Harkness',
+    //   email: 'jack@torchwood.com',
+    //   sendCatalog: false
+    // })
+
+    // Use patchValue when setting a subset of the values in a form
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      sendCatalog: false
+    })
+  }
+
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
